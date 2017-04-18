2d84fc9058263f1ea133b89174dab28ecfbc881a
shobanyogi <shobanyogi1996@gmail.com>
2017-04-18 12:30:09 +0530

    Add files via upload

---
+++ camel.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+#include<string.h>
+
+int main()
+{
+    
+char a[20];
+    
+int b,i;
+    
+printf("Enter the string");
+   
+ scanf("%s",a);
+    
+b=strlen(a);
+    
+for(i=0;i<b;i++)
+    
+{
+        
+if(i==0)
+       
+ {
+           
+ printf("%s",a[i]-32);
+       
+ }
+        
+else
+       
+ {
+           
+ printf("%s",a[i]);
+       
+ }
+   
+ }
+
+return 0;
+}
