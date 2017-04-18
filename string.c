1ef91eb8fb43cd8d8439c359c2cbc58b9f4e1dea
shobanyogi <shobanyogi1996@gmail.com>
2017-04-18 12:07:46 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    
+char a[20];
+    
+int b;
+    
+printf("Enter the string");
+    
+scanf("%s",a);
+    
+b=atoi(a);
+    
+printf("%d",b);
+
+return 0;
+}
